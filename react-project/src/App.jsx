0df693918dfc485ee031fe2333ff912c907c583a
codewithashfaque <mohammadashfaque18@gmail.com>
2026-04-23 22:36:19 +0530

    add routing

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
+  import React from 'react';
+  import {Route , Routes} from "react-router-dom";
+  import Home from './components/Home';
 
 
-const App = () => {
-  return (
-    <div className='h-screen w-screen'>
-      <nav className='w-[15%] h-full bg-zinc-50 flex flex-col items-center pt-5'>
-        <a className='px-5 py-2 border rounded border-blue-200 text-blue-300'
-        href='/create'>
-          Add new  Products
-        </a>
-      </nav>
-    </div>
-  )
-}
+  const App = () => {
+    return (
+      <div className='h-screen w-screen flex'>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+      </div>
+    )
+  }
 
-export default App
+  export default App
