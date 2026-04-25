cac601eb6f50609adb81e6f003e970edd0929b96
codewithashfaque <mohammadashfaque18@gmail.com>
2026-04-25 10:39:19 +0530

    catgeorization the product and use filter

--- react-project/src/components/Nav.jsx
+++ react-project/src/components/Nav.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ProductContext } from '../utils/Context';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
+
+    const [products] = useContext(ProductContext);
+
+    let distinct_category =
+       products && products.reduce((acc, cv) => [...acc, cv.category], [])
+    distinct_category = [...new Set(distinct_category)]
+    
+    const color = () => {
+      return `rgba(${(Math.random()*255).toFixed()} , ${(Math.random()*255).toFixed()} , ${(Math.random()*255).toFixed()}, 0.4)`;
+    }
   return (
     <nav className='w-[15%] h-full bg-zinc-50 flex flex-col items-center pt-5'>
           <a className='px-5 py-2 border rounded border-blue-200 text-blue-300'
@@ -9,19 +21,16 @@ const Nav = () => {
           </a>
           <hr className='my-3  w-[80%]'/>
           <h1 className='text-2xl mb-3 w-[80%]'>Category Filter</h1>
-          <ul className=' w-[80%]'>
-            <li className=' flex items-center  mb-3'>
-              <span className='rounded-full mr-2 w-[15px] h-[15px] bg-blue-100 '></span>{" "}cat 1</li>
-              <li className=' flex items-center  mb-3'>
-              <span className='rounded-full mr-2 w-[15px] h-[15px] bg-red-100 '></span>{" "}cat 2</li>
-              <li className=' flex items-center  mb-3'>
-              <span className='rounded-full mr-2 w-[15px] h-[15px] bg-green-100 '></span>{" "}cat 3</li>
-              <li className=' flex items-center  mb-3'>
-              <span className='rounded-full mr-2 w-[15px] h-[15px] bg-yellow-100 '></span>{" "}cat 4</li>
-              
-          
+          <div className=' w-[80%]'>
 
-          </ul>
+            {distinct_category.map((c,i) =>(
+
+              <Link key={i} to={`/?category=${c}`} className=' flex items-center mb-3'>
+              <span style={{backgroundColor: color()}} className='rounded-full mr-2 w-[15px] h-[15px]'></span>{" "}{c}</Link> 
+
+            ))}
+
+          </div>
         </nav>
   )
 }
